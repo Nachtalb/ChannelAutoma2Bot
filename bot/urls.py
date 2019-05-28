79c1571bfe15e0167260cf4b061e9ac1bbf81a30
Nick Espig <nickespig@gmail.com>
2019-05-29 00:15:33 +0200

    Add redirect to admin panel

--- bot/urls.py
+++ bot/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+
+from bot.views import redirec_to_admin_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', redirec_to_admin_view),
     url(r'^', include('django_telegrambot.urls')),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from django.shortcuts import redirect
+
+
+def redirec_to_admin_view(request):
+    return redirect('/admin')
