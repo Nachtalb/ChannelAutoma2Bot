f174ac0296c2d25ed302088ab793b052538508a2
Nick Espig <nickespig@gmail.com>
2019-08-07 23:47:55 +0200

    Set fontsize dynamically
    By default it is set to use up about 50% of the images width.

--- bot/utils/media.py
+++ bot/utils/media.py
@@ -30,14 +30,14 @@ def watermark_text(in_image: IO or str or Path,
                    pos: Tuple[int, int] = None,
                    colour: Tuple[int, int, int] = None,
                    font_name: str = None,
-                   font_size: int = None):
+                   font_size: int = None,
+                   font_size_percentage: int = None):
     pos = pos or (0, 0)
     colour = colour or (0, 0, 0)
     if font_name and font_name not in available_fonts:
         raise KeyError(f'Font "{font_name}" not available')
     font_name = font_name or 'default'
     font_path = available_fonts.get(font_name, available_fonts['default'])['path']
-    font_size = font_size or 20
 
     if not isinstance(out_buffer, (str, Path)) and not file_extension:
         raise AttributeError('If out_image is a Buffer file_extension must be set')
@@ -45,7 +45,19 @@ def watermark_text(in_image: IO or str or Path,
     photo = Image.open(in_image)
     drawing = ImageDraw.Draw(photo)
 
-    font = ImageFont.truetype(font_path, font_size)
+    if not font_size or font_size_percentage:
+        font_size_percentage = font_size_percentage or 50
+        fontsize = 1
+        img_fraction = font_size_percentage / 100
+
+        font = ImageFont.truetype(font_path, fontsize)
+        while font.getsize(text)[0] < img_fraction * photo.size[0]:
+            fontsize += 1
+            font = ImageFont.truetype(font_path, fontsize)
+        fontsize -= 1  # Fontsize shall be one less than defined fraction
+        font = ImageFont.truetype(font_path, fontsize)
+    else:
+        font = ImageFont.truetype(font_path, font_size)
 
     drawing.text(pos, text, fill=colour, font=font)
     photo.save(out_buffer, format='webp', lossless=True)
