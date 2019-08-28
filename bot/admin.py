8de614d02b3c7f7f8a6ee83b597c255093688534
Nick Espig <nickespig@gmail.com>
2019-08-28 23:35:19 +0200

    Improve admin panel by a lot

--- bot/admin.py
+++ bot/admin.py
@@ -1,25 +1,83 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.safestring import SafeText, mark_safe
 
 from bot.models.channel_settings import ChannelSettings
 from bot.models.reactions import Reaction
 from bot.models.usersettings import UserSettings
 
+link_template = '<a href="{link}" target={target}>{text}</a>'
+
+
+def admin_channel_link(channel: ChannelSettings) -> SafeText:
+    url = reverse(f'admin:bot_channelsettings_change', args=(channel.channel_id,))
+    return format_html(link_template, link=url, text=channel.name, target='_self')
+
+
+def admin_user_link(user: UserSettings) -> SafeText:
+    url = reverse(f'admin:bot_usersettings_change', args=(user.user_id,))
+    return format_html(link_template, link=url, text=user.name, target='_self')
+
 
 class UserSettingsAdmin(admin.ModelAdmin):
-    list_display = ['user_id', 'username', 'user_fullname', 'channel__names', 'modified', 'created']
+    fieldsets = (
+        ('Infos', {
+            'fields': ('user_id', 'username', 'user_fullname')
+        }),
+        ('Bot State', {
+            'fields': ('_user_state', 'current_channel'),
+        }),
+    )
+
+    list_display = ['user_id', 'username_tg', 'user_fullname', 'channel__names', 'current_channel__link', 'modified', 'created']
     list_filter = ['channels']
 
-    def channel__names(self, obj):
-        return list(map(lambda o: o.name, obj.channels.all()))
+    def current_channel__link(self, obj: UserSettings) -> SafeText or None:
+        if obj.current_channel:
+            return admin_channel_link(obj.current_channel)
+        return
+
+    def username_tg(self, obj: UserSettings) -> SafeText or str:
+        if obj.user and obj.user.link:
+            return format_html(link_template, link=obj.user.link, text=obj.name, target='_blank')
+        return obj
+
+    def channel__names(self, obj: UserSettings) -> SafeText:
+        return mark_safe(', '.join(map(admin_channel_link, obj.channels.all())))
 
 
 admin.site.register(UserSettings, UserSettingsAdmin)
 
 
 class ChannelSettingsAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Infos', {
+            'fields': ('channel_id', 'channel_username', 'channel_title')
+        }),
+        ('User', {
+            'fields': ('added_by', 'users'),
+        }),
+        ('Reaction', {
+            'fields': ('_reactions',),
+        }),
+        ('Captions', {
+            'fields': ('caption', 'image_caption', 'image_caption_font', 'image_caption_direction'),
+        }),
+    )
+
+
     list_display = [
-        'channel_id', 'name', 'added_by', 'caption_small', 'image_caption_small', 'reactions', 'modified', 'created'
+        'channel_id', 'channel_tg', 'channel_title', 'added_by_user', 'caption_small', 'image_caption_small', 'reactions', 'modified', 'created'
     ]
+
+    def channel_tg(self, obj: ChannelSettings) -> SafeText or str:
+        if obj.chat and obj.chat.link:
+            return format_html(link_template, link=obj.chat.link, text=obj.name, target='_blank')
+        return obj.name
+
+    def added_by_user(self, obj: ChannelSettings) -> SafeText or str:
+        return admin_user_link(obj.added_by)
 
     def caption_small(self, obj):
         if not obj.caption or len(obj.caption) <= 100:
@@ -36,13 +94,11 @@ admin.site.register(ChannelSettings, ChannelSettingsAdmin)
 
 
 class ReactionsAdmin(admin.ModelAdmin):
-    list_display = ['reaction', 'message', 'channel__name', 'users__count', 'created']
+    list_display = ['reaction', 'message', 'channel_link', 'users__count', 'created']
     list_filter = ['channel']
 
-    def channel__name(self, obj):
-        return obj.channel.name
-
-    channel__name.admin_order_field = 'channel__name'
+    def channel_link(self, obj: Reaction) -> SafeText:
+        return admin_channel_link(obj.channel)
 
     def users__count(self, obj):
         return obj.users.count()
